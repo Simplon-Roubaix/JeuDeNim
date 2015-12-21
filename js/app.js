1bea7e14611aff163fbbcaf03adcb7b67848e15a
didierClin <didier.c62@numericable.fr>
2015-12-21 19:17:41 +0100

    Ajout event listener sur bouton demarrer et affichage d'un message test.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
 $(document).foundation();
 
+// 1 : recuperation des elements du DOM
 var cibleEl = document.getElementById("cible");
+var clicCommencer = document.getElementById("commencer");
 
-cibleEl.textContent = "Je m'affiche ici";
+// 2 : les fonctions
+var onClicButt = function(){
+    cibleEl.textContent = "Je m'affiche ici";
+    
+}
+
+// 3 : addEvent
+clicCommencer.addEventListener("click",onClicButt); 
+
