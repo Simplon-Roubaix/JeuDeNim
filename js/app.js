9db54f5a57e4dfa45c94ce7ed275650c9d0431df
didierClin <didier.c62@numericable.fr>
2015-12-22 18:01:03 +0100

    Correction du nom des fonctions onClickCommencer et jeuDeNim

--- js/app.js
+++ js/app.js
@@ -9,33 +9,40 @@ var clicCommencer = document.getElementById("commencer");
 alert(partie);
 
 // 2 : les fonctions
-var onImgClic = function (n) {
-        // Partie ouverte? 
-        /*
-        */
-        {
-        b0El.src = "img/allumette-brulee.png";
-        b1El.src = "img/allumette-brulee.png";
-        }
-    }
 // fonction pour changer l'objet choisi
-function choisie(event) {
+function jeuDeNim(event) {
      
         //this.style.backgroundColor = 'black';
         this.src = "img/allumette-brulee.png";
     
 }
 
-function jeuDeNim(){
-    
+
+
+
+function onclickCommencer(){
     partie = 1;
     alert(partie);
-    document.getElementById("b0").onclick = choisie;
-    
+    document.getElementById("b0").onclick = jeuDeNim;
+    document.getElementById("b1").onclick = jeuDeNim;
+    document.getElementById("b2").onclick = jeuDeNim;
+    document.getElementById("b3").onclick = jeuDeNim;
+    document.getElementById("b4").onclick = jeuDeNim;
+    document.getElementById("b5").onclick = jeuDeNim;
+    document.getElementById("b6").onclick = jeuDeNim;
+    document.getElementById("b7").onclick = jeuDeNim;
+    document.getElementById("b8").onclick = jeuDeNim;
+    document.getElementById("b9").onclick = jeuDeNim;
+    document.getElementById("b10").onclick = jeuDeNim;
+    document.getElementById("b11").onclick = jeuDeNim;
+    document.getElementById("b12").onclick = jeuDeNim;
+    document.getElementById("b13").onclick = jeuDeNim;
+    document.getElementById("b14").onclick = jeuDeNim;
+    document.getElementById("b15").onclick = jeuDeNim;
 }
 
 // 3 : addEvent
-clicCommencer.addEventListener("click",jeuDeNim); 
+clicCommencer.addEventListener("click",onclickCommencer); 
 
 
     
