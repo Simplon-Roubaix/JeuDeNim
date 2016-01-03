8a153add12e8512adca5bdb26edd9152b15dd6af
didierClin <didier.c62@numericable.fr>
2016-01-04 00:00:36 +0100

    mise en place plateau de jeu et controle des joueurs

--- js/app.js
+++ js/app.js
@@ -1,18 +1,50 @@
 $(document).foundation();
 
 // les variables:
+// partie compte les coups - arrivé à 16 fin du jeu
+// name1 et 2 pour les noms des joueurs
+// n joueur courant
+// compte permet un max de 3 coups par joueur
 var partie = 0, name1,name2,n=1,compte=3;
-
+//  Jeu stocke le maps du jeu en cours
+var jeu = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1];
 // 1 : recuperation des elements du DOM
+// enplacement du tableau de jeu
 var cibleEl = document.getElementById("cible");
+// bouton pour lancer le jeu
 var clicCommencer = document.getElementById("commencer");
-
-
-var rJeuEl = document.getElementById("rJeu");
+// zone affichage des messages interactifs pendant la partie
 var messEl = document.getElementById("texte");
 
 
 // 2 : les fonctions
+// fonction qui gere l'affichage du jeu
+// recoit la var tableau jeu à chaque entrée joueur
+var affiche = function(table) {
+    var nbBat = 0;
+    for (var j=0; j<table.length;j++){
+       // change le contenu de la case choisie et sa classe
+        var elt = document.getElementById("b"+j);
+        // l'element est-il existant? oui je modifie ,
+        if (table[j]===1)  {
+                elt.src = "img/baton.png";
+                // elt.className = "button";
+                // elt.className += " rempli";
+                nbBat++;
+            }
+        else  {
+                elt.src = "img/allumette-brulee.png";
+                // elt.className = "button";
+                // elt.className += " vide";
+            }
+    }
+    //fin de partie
+    if (nbBat===2) partie = false;
+};
+
+// affichage initial du jeu
+affiche(jeu);
+
 // fonction pour changer l'objet choisi
 function jeuDeNim(event) {
 
@@ -92,9 +124,9 @@ function changeJoueur() {
 function onclickCommencer(){
   // champs nom de joueur
   name1 = document.getElementById("nomJ1").value;
-console.log("--" + name1 + "--");
+// console.log("--" + name1 + "--");
   name2 = document.getElementById("nomJ2").value;
-console.log("--" + name2 + "--");
+// console.log("--" + name2 + "--");
 // choix du type de joueur et qui jouera 1er
  var joueur1 = document.getElementById("j1").value;
  while (joueur1 === "Joueur1" && !name1) {
@@ -103,12 +135,13 @@ console.log("--" + name2 + "--");
    return;
  }
  var joueur2 = document.getElementById("j2").value;
- while (joueur2 === "Joueur2" && !name2) {
-   messEl.textContent = "Veuillez saisir votre prénom joueur2";
+ while (  joueur1 === joueur2  || joueur2 === "Joueur2" && !name ) {
+   messEl.textContent = "Veuillez saisir votre prénom joueur2 ou modifiez l'un des joueurs !";
+   messEl.className += " active";
    return;
  }
  messEl.textContent = "Veuillez choisir de 1 à 3 batons " + name1;
- messEl.textContent += " Vous affrontez " + name2;
+ messEl.textContent += " vous affrontez " + (name2 || "l'ordinateur");
 
 // console.log(name2);
     // messEl.textContent = "A vous de débuter "+ name1;
@@ -116,6 +149,7 @@ console.log("--" + name2 + "--");
     partie = 16;
 
     console.log(partie);
+    affiche(jeu);
     // On detecte les clics sur les batons
     // document.getElementById("b0").onclick = jeuDeNim;
 
